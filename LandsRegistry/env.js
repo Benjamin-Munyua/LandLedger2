78a58c4e2af4201c180311e7410588e1d8987c28
Benjamin-Munyua <benjaminmunyuag@gmail.com>
2025-05-23 14:07:36 +0300

    Delete LandsRegistry directory

--- LandsRegistry/env.js
+++
@@ -1,8 +0,0 @@
-// env.js - Environment variables for frontend
-
-const ENV = {
-    CONTRACT_ADDRESS: '0xeD53156a81A8B7bE911FcEDDeEc3E8805D44ef01', // From Backend/.env
-    ALCHEMY_API_KEY: 'ysI8uYMiKU34KDgsREWRYPnCpGmH4RBA', // From Backend/.env
-    ALCHEMY_RPC_URL: 'https://polygon-amoy.g.alchemy.com/v2/ysI8uYMiKU34KDgsREWRYPnCpGmH4RBA',
-    CHAIN_ID: 80002, // Polygon Amoy testnet
-}; 
